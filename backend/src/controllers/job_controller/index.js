c42ae39b92964bc67ef05553e39a332ea79b0678
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-09-17 13:38:10 -0300

    Update index.js
    Added exports for getJobBySlug, updateJob, and deleteJob controllers to index.js for improved modular access to job-related operations.

--- backend/src/controllers/job_controller/index.js
+++ backend/src/controllers/job_controller/index.js
@@ -1,2 +1,5 @@
 export { default as createJob } from "./create_job.controller.js";
-export { default as listJobs }  from "./list_jobs.controller.js";
+export { default as listJobs } from "./list_jobs.controller.js";
+export { default as getJobBySlug } from "./get_job_by_slug.controller.js";
+export { default as updateJob } from "./update_job.controller.js";
+export { default as deleteJob } from "./delete_job.controller.js";
